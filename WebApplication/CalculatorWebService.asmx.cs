8d6db1fcd522a330d432f70a45a4b9a18200ecad
Kesiu <kesiu.pro@gmail.com>
2020-07-26 16:53:53 +0200

    Initial commit

---
+++ WebApplication/CalculatorWebService.asmx.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Services;
+
+namespace WebApplication
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")] // Can be any string but its common to give it internet domain name. Uniquely identify webservice form other webservices which are already on the web.
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
