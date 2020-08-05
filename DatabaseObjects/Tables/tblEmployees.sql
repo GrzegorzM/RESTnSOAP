134c4de497ad9985b778b19deada797eb63747c7
Kesiu <kesiu.pro@gmail.com>
2020-08-05 23:31:35 +0200

    WebApi SQL Server

---
+++ DatabaseObjects/Tables/tblEmployees.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblEmployees]
+(
+	id int primary key identity(1, 1),
+    FirstName nvarchar(50) not null,
+    LastName nvarchar(50) not null,
+    Gender nvarchar(50) not null,
+    Salary int not null
+)
---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public List<tblEmployees> Get()
+        {
+            List<tblEmployees> employees;
+            using(Entities db = new Entities())
+            {
+                employees = db.tblEmployees.ToList();
+            }
+            return employees;
+        }
+
+        public tblEmployees Get(int id)
+        {
+            using (Entities db = new Entities())
+            {
+                return db.tblEmployees.FirstOrDefault(x => x.ID == id);
+            }
+        }
+
+        public void Put(tblEmployees employeeModyfied)
+        {
+            using (Entities db = new Entities())
+            {
+                tblEmployees employee = db.tblEmployees.Find(employeeModyfied.ID);
+                employee.FirstName = employeeModyfied.FirstName;
+                employee.LastName = employeeModyfied.LastName;
+                employee.Gender = employeeModyfied.Gender;
+                employee.Salary = employeeModyfied.Salary;
+
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void Post(tblEmployees employee)
+        {
+            using (Entities db = new Entities())
+            {
+                db.tblEmployees.Add(employee);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (Entities db = new Entities())
+            {
+                tblEmployees employee = db.tblEmployees.Find(id);
+                if (employee != null)
+                {
+                    db.tblEmployees.Remove(employee);
+                }
+            }
+        }
+    }
+}
