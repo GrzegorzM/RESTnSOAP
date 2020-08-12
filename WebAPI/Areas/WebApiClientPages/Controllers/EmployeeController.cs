d571e91f8500a85419463db04eed1f4c37ff5638
Kesiu <kesiu.pro@gmail.com>
2020-08-13 01:30:50 +0200

    Web Api - Ajax call

---
+++ WebAPI/Areas/WebApiClientPages/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace WebAPI.Areas.WebApiClientPages.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployees()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ WebAPI/Areas/WebApiClientPages/WebApiClientPagesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebAPI.Areas.WebApiClientPages
+{
+    public class WebApiClientPagesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WebApiClientPages";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WebApiClientPages_default",
+                "WebApiClientPages/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
