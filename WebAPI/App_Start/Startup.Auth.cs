f0b306dacbee66795959138028667c586af058a0
Kesiu <kesiu.pro@gmail.com>
2020-09-15 12:11:10 +0200

    Log off functionality

--- WebAPI/App_Start/Startup.Auth.cs
+++ WebAPI/App_Start/Startup.Auth.cs
@@ -44,7 +44,7 @@ namespace WebAPI
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14), //).FromSeconds(10),
                 // In production mode set AllowInsecureHttp = false
                 AllowInsecureHttp = true
             };
