190460886857be015773b1a924d825a60987a71b
Kesiu <kesiu.pro@gmail.com>
2020-10-14 15:37:09 +0200

    SessionMode - NotAllowed, Required, Allowed

--- WCFproject/ISimpleService.cs
+++ WCFproject/ISimpleService.cs
@@ -2,7 +2,16 @@
 
 namespace WCFproject
 {
-    [ServiceContract]
+    //[ServiceContract]
+
+    //Service contract does not support bindings that initiate sessions.
+    //[ServiceContract(SessionMode = SessionMode.NotAllowed)]
+
+    //Service contract requires a binding that supports session. Exception is thrown if the binding does not support sessions.
+    //[ServiceContract(SessionMode = SessionMode.Required)]
+    
+    //Service contract supports sessions if the binding supports them.
+    [ServiceContract(SessionMode = SessionMode.Allowed)]
     public interface ISimpleService
     {
         [OperationContract]
